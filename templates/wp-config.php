af938a38b71f481cd64f0e48b8c8871307c7669c
weizhang2017 <wzhang@leadbook.com>
2018-10-01 23:34:51 +0800

    fixed database user priv issue

--- templates/wp-config.php
+++ templates/wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'wpuser');
 define('DB_PASSWORD', '123password123');
 
 /** MySQL hostname */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');

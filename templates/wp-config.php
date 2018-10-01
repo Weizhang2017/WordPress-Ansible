5e90097782d153ed21f0ac5b31d07374fea16fff
weizhang2017 <wzhang@leadbook.com>
2018-10-01 22:38:10 +0800

    fixed db connection error

--- templates/wp-config.php
+++ templates/wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'wpuser');
 define('DB_PASSWORD', '123password123');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
